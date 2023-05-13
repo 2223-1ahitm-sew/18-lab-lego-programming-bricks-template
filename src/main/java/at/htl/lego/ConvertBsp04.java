0d943caf39b72d02173291b6098cd9decadfc6d8
htl-leonding <t.stuetz@htl-leonding.ac.at>
2023-05-13 12:40:40 +0200

    doc(methods): add bsp01

---
+++ src/main/java/at/htl/lego/ConvertBsp04.java
@@ -0,0 +1,18 @@
+package at.htl.lego;
+
+public class ConvertBsp04 {
+
+    public static void main(String[] args) {
+        System.out.println(convertDigitCharToInt('4'));
+    }
+
+    static int convertDigitCharToInt(char c) {
+
+        if (c >= '0' && c <= '9') {
+            return c - '0';
+        }
+        return -1;
+    }
+
+
+}
