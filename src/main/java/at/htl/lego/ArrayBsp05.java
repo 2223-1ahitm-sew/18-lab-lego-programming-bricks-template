910d25559abc63823ab48f384d07e8bfde34d2eb
htl-leonding <t.stuetz@htl-leonding.ac.at>
2023-05-18 19:11:10 +0200

    solutions: add ArrayBsp06.java

---
+++ src/main/java/at/htl/lego/ArrayBsp05.java
@@ -0,0 +1,12 @@
+package at.htl.lego;
+
+import java.util.Arrays;
+
+public class ArrayBsp05 {
+
+    public static void main(String[] args) {
+        String[] name = {"Dagobert", "", "Daisy", "Gundel", ""};
+        System.out.println(Arrays.toString(name));
+    }
+
+}
